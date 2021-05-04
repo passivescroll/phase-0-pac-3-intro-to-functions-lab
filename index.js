28385ae7cfb4ef1da1ab2b85aa3a3c46d6699469
sky <flahertysky12@gmail.com>
2021-05-04 15:59:28 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-function shout(string) {
+  function shout(string) {
     return string.toUpperCase(); //'Hello!'
-}
-
-function whisper(string){
-    return string.toLowerCase(); //'hello'
   }
 
-  function logShout(string){
-    return console.log(string.toUpperCase()) //'Hello!'
+  function whisper(string) {
+    return string.toLowerCase()
   }
 
-
-  function logWhisper(string){
-    return console.log(string.toLowerCase()) //'hello'
+  function logShout(string) {
+    return console.log(string.toUpperCase());
   }
 
-  
-  
-  
+  function logWhisper(string) {
+    return console.log(string.toLowerCase());
+  }
+
   function sayHiToGrandma(string){
-    if (string === string.toLowerCase()) return "I can't hear you!";
-  else if (string === "I love you, Grandma.") return "I love you, too.";
-  else if (string === string.toUpperCase()) return "YES INDEED!";
-
+    if (string === string.toLowerCase()) return "I can't hear you!"
+    else if (string === string.toUpperCase()) return "YES INDEED!"
+    else if (string === "I love you, Grandma.") return "I love you, too."
   }
