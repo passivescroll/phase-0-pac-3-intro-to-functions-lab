5ac50e26fdd14e563be0c5f28d9d26be081bc37f
sky <flahertysky12@gmail.com>
2021-05-04 17:45:30 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -18,4 +18,4 @@
     if (string === string.toLowerCase()) return "I can't hear you!"
     else if (string === string.toUpperCase()) return "YES INDEED!"
     else if (string === "I love you, Grandma.") return "I love you, too."
-  }
+  } 
